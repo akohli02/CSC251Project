f78af68b07b9aca33241f5998bc54b9da4f133f7
Aimy Kohli <akohli02@email.cpcc.edu>
2021-11-01 15:29:00 -0400

    Moved fields and methods to the AirConditioner

--- AirConditioner.java
+++ AirConditioner.java
@@ -1,6 +1,68 @@
 public class AirConditioner
 {
+   //Fields
+   
+      private String manufacturer;
+      private String type;
+      private double coolingCapacity;
+      
+   /**
+      Constructor
+      @param manufacturer
+      @param type
+      @param coolingCapacity
+      
+   */
+   
+      public AirConditioner()
+      {
+      
+      }
+      
+      public AirConditioner( String manufacturer, String type, double coolingCapacity)
+      {
+         this.manufacturer = manufacturer;
+         this.type = type;
+         this.coolingCapacity = coolingCapacity;
+      }
+      
+   //Mutators
+      public void setManufacturer( String manufacturer)
+      {
+         this.manufacturer = manufacturer;
+      }
+   
+      public void setType(String type)
+      {
+         this.type = type;
+      }
+      
+      public void setCoolingCapacity( double coolingCapacity)
+      {
+         this.coolingCapacity = coolingCapacity;
+      }
+      
+      //Accessors
+      public String getManufacturer()
+      {
+         return manufacturer;
+      }
+   
+      public String getType()
+      {
+         return type;
+      }
+      
+      public double getCoolingCapacity()
+      {
+         return coolingCapacity;
+      }
 
-
-
+      public String toString()
+      {
+         String str = "Air Conditioner Manufacturer: " + manufacturer
+                       + "\nAir Conditioner Type: " + type
+                       + "\nAir Conditioner Cooling Capacity (BTUs Per Hour): " + coolingCapacity;
+         return str;
+      }
 }
